adc3039c6b98f6034c7673a1cb34daa2f62854e6
kuriakosant <kuriakosant2003@gmail.com>
2024-10-17 12:00:00 +0300

    create pdf page for espa document

---
+++ src/pages/PDFPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PDFButton from '../components/ui/PDFButton';
+
+const PDFPage: React.FC = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">ESPA</h1>
+      <PDFButton pdfUrl="/src/assets/document.pdf" buttonText="View Document" />
+      {/* Other content */}
+    </div>
+  );
+};
+
+export default PDFPage;
+
