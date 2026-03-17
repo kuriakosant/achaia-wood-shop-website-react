08fb2693051af8ce573cf026854a52344d381c25
kuriakosant <kuriakosant2003@gmail.com>
2026-03-17 14:46:53 +0200

    fix: remove unused Download import

--- src/components/ESPAViewer.tsx
+++ src/components/ESPAViewer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { X, Download } from 'lucide-react';
+import { X } from 'lucide-react';
 
 // Import the image directly
 import EspaImage from 'assets/ESPA-IMAGE.png';
